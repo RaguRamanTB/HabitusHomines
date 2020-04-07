34b79826a164a841069b5ad4b9ec6d2929eb4561
RaguRamanTB <aakash1956ragu@gmail.com>
2020-04-07 11:02:05 +0530

    Initial commit

---
+++ app/src/main/java/com/android/coronahack/heedcustomer/MainActivity.java
@@ -0,0 +1,17 @@
+package com.android.coronahack.heedcustomer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+
+    }
+}
