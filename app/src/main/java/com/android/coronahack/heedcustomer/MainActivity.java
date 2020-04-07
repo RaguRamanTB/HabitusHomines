2ebe73209b675f4643233f986c9a9de9fe76b8fa
RaguRamanTB <aakash1956ragu@gmail.com>
2020-04-07 13:17:52 +0530

    Registration completed

--- app/src/main/java/com/android/coronahack/heedcustomer/MainActivity.java
+++ app/src/main/java/com/android/coronahack/heedcustomer/MainActivity.java
@@ -1,8 +1,12 @@
 package com.android.coronahack.heedcustomer;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        startRegistration();
 
+    }
+
+    private void startRegistration() {
+        ViewGroup viewGroup = findViewById(android.R.id.content);
+        View registerView = LayoutInflater.from(this).inflate(R.layout.register_layout, viewGroup, false);
+        new AlertDialog.Builder(this)
+                .setView(registerView)
+                .setCancelable(false)
+                .create().show();
     }
 }
