e865920ca03ed18b2a79ec6096306ddac85794bd
RaguRamanTB <aakash1956ragu@gmail.com>
2020-04-09 15:37:59 +0530

    Groceries layout finished!

--- app/src/main/java/com/android/coronahack/heedcustomer/activities/GroceryStoreActivity.java
+++ app/src/main/java/com/android/coronahack/heedcustomer/activities/GroceryStoreActivity.java
@@ -1,16 +1,97 @@
 package com.android.coronahack.heedcustomer.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.android.coronahack.heedcustomer.R;
+import com.android.coronahack.heedcustomer.helpers.EnterMedAdapter;
+import com.android.coronahack.heedcustomer.helpers.EnterMeds;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroceryStoreActivity extends AppCompatActivity {
+
+    ImageView locateG, addG, removeG;
+    EditText nearestG, gName, gQuantity;
+    Button submitG;
+    RecyclerView gRecycler;
+    RecyclerView.Adapter gAdapter;
+    List<EnterMeds> enterGList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grocery_store);
+
+        Toolbar toolbar = findViewById(R.id.buyGrocery);
+        toolbar.setTitle("Buy Grocery");
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        locateG = findViewById(R.id.gLocationButton);
+        addG = findViewById(R.id.gPlus);
+        removeG = findViewById(R.id.gMinus);
+        nearestG = findViewById(R.id.nearestG);
+        gName = findViewById(R.id.gName);
+        gQuantity = findViewById(R.id.gQuantity);
+        submitG = findViewById(R.id.submitGrocery);
+        gRecycler = findViewById(R.id.gRecycler);
+
+        enterGList = new ArrayList<>();
+        gRecycler.setLayoutManager(new LinearLayoutManager(this));
+
+        addG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addGroceries();
+            }
+        });
+
+        removeG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeGroceries();
+            }
+        });
+    }
+
+    private void removeGroceries() {
+        if (enterGList.size() > 0) {
+            enterGList.remove(enterGList.size() - 1);
+            gAdapter = new EnterMedAdapter(this, enterGList);
+            gRecycler.setAdapter(gAdapter);
+        }
+    }
+
+    private void addGroceries() {
+        String getGName, getGQuantity;
+        getGName = gName.getText().toString();
+        getGQuantity = gQuantity.getText().toString();
+
+        if (getGName.equals("") || getGName.length() == 0
+                || getGQuantity.equals("") || getGQuantity.length() == 0) {
+            Toast.makeText(GroceryStoreActivity.this, "Enter both item name and quantity", Toast.LENGTH_SHORT).show();
+        } else {
+            enterGList.add(new EnterMeds(getGName, getGQuantity));
+            gAdapter = new EnterMedAdapter(this, enterGList);
+            gRecycler.setAdapter(gAdapter);
+            gName.setText("");
+            gQuantity.setText("");
+        }
     }
 }
--- app/src/main/java/com/android/coronahack/heedcustomer/activities/MedicalShopActivity.java
+++ app/src/main/java/com/android/coronahack/heedcustomer/activities/MedicalShopActivity.java
@@ -1,11 +1,24 @@
 package com.android.coronahack.heedcustomer.activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,17 +29,26 @@ import com.android.coronahack.heedcustomer.R;
 import com.android.coronahack.heedcustomer.helpers.EnterMedAdapter;
 import com.android.coronahack.heedcustomer.helpers.EnterMeds;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class MedicalShopActivity extends AppCompatActivity {
 
-    ImageView locateMed, addTab, removeTab, uploadPrescription;
-    EditText nearestMed, uploadedPrescription, tabName, tabQuantity;
+    ImageView locateMed, addTab, removeTab, uploadPrescription, uploadedPrescription;
+    EditText nearestMed, tabName, tabQuantity;
     Button submit;
     RecyclerView tabsRecycler;
     RecyclerView.Adapter mAdapter;
     List<EnterMeds> enterMedsList;
+    private static final int PERMISSION_REQUEST = 0;
+    private static final int RESULT_LOAD_IMAGE = 1855;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +70,7 @@ public class MedicalShopActivity extends AppCompatActivity {
         removeTab = findViewById(R.id.tabMinus);
         uploadPrescription = findViewById(R.id.uploadPrescriptionButton);
         nearestMed = findViewById(R.id.nearestMed);
-        uploadedPrescription = findViewById(R.id.uploadPrescription);
+        uploadedPrescription = findViewById(R.id.uploadedPrescriptionImage);
         tabName = findViewById(R.id.tabName);
         tabQuantity = findViewById(R.id.tabQuantity);
         submit = findViewById(R.id.submit);
@@ -70,6 +92,46 @@ public class MedicalShopActivity extends AppCompatActivity {
                 removeTablets();
             }
         });
+
+        uploadPrescription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickImage();
+            }
+        });
+    }
+
+    private void clickImage() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST);
+        } else {
+            Intent intent = new Intent (android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent, RESULT_LOAD_IMAGE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_REQUEST) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, RESULT_LOAD_IMAGE);
+            } else {
+                Toast.makeText(this,"Permission Denied! Please restart the app to use this feature.",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ((requestCode == RESULT_LOAD_IMAGE) && (resultCode == Activity.RESULT_OK)) {
+            assert data != null;
+            Bitmap photo = (Bitmap) Objects.requireNonNull(data.getExtras()).get("data");
+            uploadedPrescription.setImageBitmap(photo);
+        }
     }
 
     private void removeTablets() {
@@ -96,4 +158,5 @@ public class MedicalShopActivity extends AppCompatActivity {
             tabQuantity.setText("");
         }
     }
+
 }
