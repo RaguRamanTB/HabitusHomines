41c55d9817952ea6b8e62f14329acdbd651fbc55
RaguRamanTB <aakash1956ragu@gmail.com>
2020-04-25 16:17:20 +0530

    Info activity layout update

--- app/src/main/java/com/android/coronahack/heedcustomer/activities/InfoActivity.java
+++ app/src/main/java/com/android/coronahack/heedcustomer/activities/InfoActivity.java
@@ -1,6 +1,7 @@
 package com.android.coronahack.heedcustomer.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -14,14 +15,22 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.INotificationSideChannel;
 import android.util.Log;
+import android.view.View;
+import android.widget.GridView;
 import android.widget.Toast;
 
 import com.android.coronahack.heedcustomer.R;
+import com.android.coronahack.heedcustomer.helpers.GridAdapter;
+import com.android.coronahack.heedcustomer.helpers.GridItem;
+
+import java.util.ArrayList;
 
 public class InfoActivity extends AppCompatActivity {
 
     Handler handler;
     private BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    GridView gridView;
+    GridAdapter gridAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +39,50 @@ public class InfoActivity extends AppCompatActivity {
 
         handler  = new Handler();
         startRepeatingTask();
+
+        Toolbar toolbar = findViewById(R.id.infoToolbar);
+        toolbar.setTitle("Learn about COVID-19");
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        gridView = findViewById(R.id.landing_grid);
+        gridAdapter = new GridAdapter(this, getData());
+        gridView.setAdapter(gridAdapter);
+    }
+
+    private ArrayList<GridItem> getData() {
+        ArrayList<GridItem> menuItems = new ArrayList<>();
+        GridItem m = new GridItem();
+        m.setGridPicture(R.drawable.masks);
+        m.setGridText("When and how to use masks");
+        menuItems.add(m);
+
+        m = new GridItem();
+        m.setGridPicture(R.drawable.myths);
+        m.setGridText("Myth-busters");
+        menuItems.add(m);
+
+        m = new GridItem();
+        m.setGridPicture(R.drawable.workplace);
+        m.setGridText("Getting workplace ready");
+        menuItems.add(m);
+
+        m = new GridItem();
+        m.setGridPicture(R.drawable.advocacy);
+        m.setGridText("Advocacy");
+        menuItems.add(m);
+
+        m = new GridItem();
+        m.setGridPicture(R.drawable.youtube);
+        m.setGridText("Watch videos");
+        menuItems.add(m);
+
+        return menuItems;
     }
 
     Runnable runnable = new Runnable() {
---
+++ app/src/main/java/com/android/coronahack/heedcustomer/helpers/GridAdapter.java
@@ -0,0 +1,70 @@
+package com.android.coronahack.heedcustomer.helpers;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.coronahack.heedcustomer.R;
+
+import java.util.ArrayList;
+
+public class GridAdapter extends BaseAdapter {
+
+    Context c;
+    ArrayList<GridItem> menuItems;
+
+    public GridAdapter(Context c, ArrayList<GridItem> menuItems) {
+        this.c = c;
+        this.menuItems = menuItems;
+    }
+
+    @Override
+    public int getCount() {
+        return menuItems.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return menuItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(c).inflate(R.layout.item_gridlayout,
+                    parent, false);
+        }
+
+        final GridItem menu = (GridItem) this.getItem(position);
+        ImageView imageView = convertView.findViewById(R.id.grid_picture);
+        TextView textView = convertView.findViewById(R.id.grid_text);
+        textView.setSelected(true);
+        textView.setText(menu.getGridText());
+        imageView.setImageResource(menu.getGridPicture());
+
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (menu.getGridText().toLowerCase()) {
+                    case "":
+                        break;
+
+                    default:
+                        Toast.makeText(c, menu.getGridText().toLowerCase(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        return convertView;
+    }
+}
